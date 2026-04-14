6fd7dae760851cbbc873f8b445c5ef2138e2dade
Nikol <ponomarevanika106@gmail.com>
2026-04-14 14:40:57 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const thickness = 0.01; // 10 мм
-const r_outer_smallest = 0.25; // 0.5 м діаметр
+const THICKNESS = 0.01; // 10 мм
+const OUTER_RADIUS_SMALLEST = 0.25; // 0.5 м діаметр
 
 let totalVolume = 0;
 
 for (let k = 1; k <= 12; k++) {
-  let r_inner = (r_outer_smallest - thickness) + (k - 1) * thickness;
-  let volume = (4 / 3) * Math.PI * Math.pow(r_inner, 3);
+  const innerRadius =
+    (OUTER_RADIUS_SMALLEST - THICKNESS) + (k - 1) * THICKNESS;
+
+  const volume = (4 / 3) * Math.PI * Math.pow(innerRadius, 3);
+
   totalVolume += volume;
 }
 
